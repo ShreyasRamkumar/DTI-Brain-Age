4015eb406f68fb33748e5a3c2f941cdc3632c582
Shreyas Ramkumar <101288239+ShreyasRamkumar@users.noreply.github.com>
2024-05-23 12:05:04 -0400

    Update Preprocessing.py

--- code/Preprocessing.py
+++ code/Preprocessing.py
@@ -1,21 +1,25 @@
 from dipy.segment.threshold import otsu
 import nibabel as nib
-from numpy import where, eye
+from numpy import where, eye, multiply
+from nilearn.masking import compute_brain_mask
+
+
 
 class Preprocessing:
     def __init__(self, image_path: str):
         self.image_path = image_path
 
     def isolate_white_matter(self):
-        # get otsu value from FA map
         image = nib.load(self.image_path)
         image_array = image.get_fdata()
-        otsu_val = otsu(image_array)
+
+        iso_mask = compute_brain_mask(target_img=image, threshold=0.1, mask_type="wm")
+        mask_array = iso_mask.get_fdata()
         
-        # use otsu value to isolate white matter
-        iso_array = where(image_array >= otsu_val, image_array, 0)
+        iso_array = multiply(image_array, mask_array)
+        
         iso_image = nib.Nifti1Image(iso_array, eye(4))
-        nib.save(iso_image, "C:/Code/GPN/DTI-Brain-Age/testing/isolated_fa_testing.nii")
+        nib.save(iso_image, "C:/Code/GPN/DTI-Brain-Age/testing/nilearn_fa_testing.nii")
 
 if __name__ == "__main__":
     p = Preprocessing("C:/Code/GPN/DTI-Brain-Age/testing/fa_testing.nii")
